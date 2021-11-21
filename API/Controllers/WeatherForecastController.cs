85ae3f6a83bedf44dcdc83dccc58643455f7584a
stefan-popovic <60715574+stefan-popovic@users.noreply.github.com>
2021-11-22 00:42:21 +0100

    End of section 2_2

--- API/Controllers/WeatherForecastController.cs
+++ API/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Logging;
 namespace API.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route(template: "[controller]")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
